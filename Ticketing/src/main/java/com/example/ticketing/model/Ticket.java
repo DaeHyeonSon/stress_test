31edb121928db70275a00d2eb17d81707d71b405
손대현 <no.7_sonny@sondaehyeon-ui-MacBookPro.local>
2024-10-10 01:05:49 +0900

    Ticketing 자바 프로젝트 추가

---
+++ Ticketing/src/main/java/com/example/ticketing/model/Ticket.java
@@ -0,0 +1,20 @@
+package com.example.ticketing.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String eventName;
+    private int availableTickets;
+}
+
